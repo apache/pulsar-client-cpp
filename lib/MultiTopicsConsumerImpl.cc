0dab2ba7288264b29166a5f54acfed257169fa9c
Yunze Xu <xyzinfernity@163.com>
2026-06-01 18:05:18 +0800

    chore: remove unnecessary WARN logs for send or receive timeouts (#584)

--- lib/MultiTopicsConsumerImpl.cc
+++ lib/MultiTopicsConsumerImpl.cc
@@ -603,14 +603,6 @@ Result MultiTopicsConsumerImpl::receive(Message& msg, int timeout) {
         if (state_ != Ready) {
             return ResultAlreadyClosed;
         }
-        auto cnx = getCnx().lock();
-        if (cnx) {
-            LOG_WARN(getName() << " Receive timeout after " << timeout << " ms, connection: "
-                               << cnx->cnxString() << ", queue size: " << incomingMessages_.size());
-        } else {
-            LOG_WARN(getName() << " Receive timeout after " << timeout
-                               << " ms, no connection, queue size: " << incomingMessages_.size());
-        }
         return ResultTimeout;
     }
 }
